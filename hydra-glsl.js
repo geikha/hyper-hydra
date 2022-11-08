2579b44820053627dc85b3ec3f4b683eb0767561
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2022-11-08 02:45:33 -0300

    update glsl

--- hydra-glsl.js
+++ hydra-glsl.js
@@ -3,130 +3,131 @@
 //by RITCHSE
 //docs: https://github.com/ritchse/hydra-extensions/blob/main/doc/hydra-glsl.md
 
-window.gS = osc().constructor.prototype
+window.gS = osc().constructor.prototype;
 
 window.glslExtension = {
-	inputArray: ()=> new Array(10)
-		.fill("i")
-		.map((x, i) => ({ name: x + i, type: "float", default: 1 })),
-	nodeCount: 0,
-	checkCode: function (code) {
-		code = code.trim();
-		lines = code.split(";");
-		ll = lines.length - 1;
-		if (!lines[ll]) { 
-			lines.pop(); 
-			ll--; 
-		}
-		lines[ll] = lines[ll].trim();
-		lines[ll] =
-			"\n" +
-			(lines[ll].substring(0, 6) != "return"
-				? "return " + lines[ll]
-				: lines[ll]) +
-			";";
-		code = lines.join(";");
-		return code;
-	},
-	getObjAndArgs: function (type, args) {
-		inputArray = false;
-		if (args[0] instanceof Array && args[0][0].constructor === String) {
-			inputArray = this.inputArray().map((x, i) => {
-				x.name = args[i] ? args[i][0] : x.name;
-				return x;
-			});
-            inputArray = inputArray.slice(0,args.length)
-			args = args.map((x) => x[1]);
-		}
-		obj = {
-			name: "glsl_ext_NODE_" + this.nodeCount,
-			type: type,
-			inputs: inputArray || this.inputArray(),
-		};
-		this.nodeCount++;
-		return [obj, args];
-	},
-	glslSource: function (code, ...args) {
-		prefix = [
-			!code.includes("vec2 uv") ? "vec2 uv = _st;\n" : '',
-			!code.includes("vec2 st") ? "vec2 st = _st;\n" : '',
-            !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : '',
-		].join('');
-		data = this.getObjAndArgs("src", args);
-		obj = data[0];
-		args = data[1];
-		obj.glsl = prefix + this.checkCode(code);
-		setFunction(obj);
-		return globalThis[obj.name](...args);
-	},
-	glslColor: function (self, code, ...args) {
-		prefix = [
-			!code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : '',
-			!code.includes("vec4 color") ? "vec4 color = _c0;\n" : '',
-		].join('');
-		data = this.getObjAndArgs("color", args);
-		obj = data[0];
-		args = data[1];
-		obj.glsl = prefix + this.checkCode(code);
-		setFunction(obj);
-		return gS[obj.name].bind(self)(...args);
-	},
-	glslCoord: function (self, code, ...args) {
-		prefix = [
-			!code.includes("vec2 uv") ? "vec2 uv = _st;\n" : '',
-			!code.includes("vec2 st") ? "vec2 st = _st;\n" : '',
-            !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : '',
-		].join('');
-		data = this.getObjAndArgs("coord", args);
-		obj = data[0];
-		args = data[1];
-		obj.glsl = prefix + this.checkCode(code);
-		setFunction(obj);
-		return gS[obj.name].bind(self)(...args);
-	},
-    glslCombine: function (self, code, texture,...args) {
-		prefix = [
-			!code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : '',
-			!code.includes("vec4 color0") ? "vec4 color0 = _c0;\n" : '',
-            !code.includes("vec4 c1") ? "vec4 c1 = _c1;\n" : '',
-			!code.includes("vec4 color1") ? "vec4 color1 = _c1;\n" : '',
-		].join('');
-		data = this.getObjAndArgs("combine", args);
-		obj = data[0];
-		args = data[1];
-        args.unshift(texture)
-		obj.glsl = prefix + this.checkCode(code);
-		setFunction(obj);
-		return gS[obj.name].bind(self)(...args)
-	},
-    glslCombineCoord: function (self, code, texture,...args) {
-		prefix = [
-			!code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : '',
-			!code.includes("vec4 color") ? "vec4 color = _c0;\n" : '',
-            !code.includes("vec2 uv") ? "vec2 uv = _st;\n" : '',
-			!code.includes("vec2 st") ? "vec2 st = _st;\n" : '',
-            !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : '',
-		].join('');
-		data = this.getObjAndArgs("combineCoord", args);
-		obj = data[0];
-		args = data[1];
-        args.unshift(texture)
-		obj.glsl = prefix + this.checkCode(code);
-		setFunction(obj);
-		return gS[obj.name].bind(self)(...args)
-	}
+  inputArray: () =>
+    new Array(10)
+      .fill("i")
+      .map((x, i) => ({ name: x + i, type: "float", default: 1 })),
+  nodeCount: 0,
+  checkCode: function (code) {
+    code = code.trim();
+    lines = code.split(";");
+    ll = lines.length - 1;
+    if (!lines[ll]) {
+      lines.pop();
+      ll--;
+    }
+    lines[ll] = lines[ll].trim();
+    lines[ll] =
+      "\n" +
+      (lines[ll].substring(0, 6) != "return"
+        ? "return " + lines[ll]
+        : lines[ll]) +
+      ";";
+    code = lines.join(";");
+    return code;
+  },
+  getObjAndArgs: function (type, args) {
+    let inputArray = false;
+    if (args[0] instanceof Array && args[0][0].constructor === String) {
+      inputArray = this.inputArray().map((x, i) => {
+        x.name = args[i] ? args[i][0] : x.name;
+        return x;
+      });
+      inputArray = inputArray.slice(0, args.length);
+      args = args.map((x) => x[1]);
+    }
+    let obj = {
+      name: "glsl_ext_NODE_" + this.nodeCount,
+      type: type,
+      inputs: inputArray || this.inputArray(),
+    };
+    this.nodeCount++;
+    return [obj, args];
+  },
+  glslSource: function (code, ...args) {
+    let prefix = [
+      !code.includes("vec2 uv") ? "vec2 uv = _st;\n" : "",
+      !code.includes("vec2 st") ? "vec2 st = _st;\n" : "",
+      !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : "",
+    ].join("");
+    let data = this.getObjAndArgs("src", args);
+    let obj = data[0];
+    args = data[1];
+    obj.glsl = prefix + this.checkCode(code);
+    setFunction(obj);
+    return globalThis[obj.name](...args);
+  },
+  glslColor: function (self, code, ...args) {
+    let prefix = [
+      !code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : "",
+      !code.includes("vec4 color") ? "vec4 color = _c0;\n" : "",
+    ].join("");
+    let data = this.getObjAndArgs("color", args);
+    let obj = data[0];
+    args = data[1];
+    obj.glsl = prefix + this.checkCode(code);
+    setFunction(obj);
+    return gS[obj.name].bind(self)(...args);
+  },
+  glslCoord: function (self, code, ...args) {
+    let prefix = [
+      !code.includes("vec2 uv") ? "vec2 uv = _st;\n" : "",
+      !code.includes("vec2 st") ? "vec2 st = _st;\n" : "",
+      !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : "",
+    ].join("");
+    let data = this.getObjAndArgs("coord", args);
+    let obj = data[0];
+    args = data[1];
+    obj.glsl = prefix + this.checkCode(code);
+    setFunction(obj);
+    return gS[obj.name].bind(self)(...args);
+  },
+  glslCombine: function (self, code, texture, ...args) {
+    let prefix = [
+      !code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : "",
+      !code.includes("vec4 color0") ? "vec4 color0 = _c0;\n" : "",
+      !code.includes("vec4 c1") ? "vec4 c1 = _c1;\n" : "",
+      !code.includes("vec4 color1") ? "vec4 color1 = _c1;\n" : "",
+    ].join("");
+    let data = this.getObjAndArgs("combine", args);
+    let obj = data[0];
+    args = data[1];
+    args.unshift(texture);
+    obj.glsl = prefix + this.checkCode(code);
+    setFunction(obj);
+    return gS[obj.name].bind(self)(...args);
+  },
+  glslCombineCoord: function (self, code, texture, ...args) {
+    let prefix = [
+      !code.includes("vec4 c0") ? "vec4 c0 = _c0;\n" : "",
+      !code.includes("vec4 color") ? "vec4 color = _c0;\n" : "",
+      !code.includes("vec2 uv") ? "vec2 uv = _st;\n" : "",
+      !code.includes("vec2 st") ? "vec2 st = _st;\n" : "",
+      !code.includes("vec2 xy") ? "vec2 xy = _st;\n" : "",
+    ].join("");
+    let data = this.getObjAndArgs("combineCoord", args);
+    let obj = data[0];
+    args = data[1];
+    args.unshift(texture);
+    obj.glsl = prefix + this.checkCode(code);
+    setFunction(obj);
+    return gS[obj.name].bind(self)(...args);
+  },
 };
 
-window.glsl = glslExtension.glslSource.bind(glslExtension)
-gS.glslColor = function(code,...args){
-    return glslExtension.glslColor(this,code,...args)
-}
-gS.glslCoord = function(code,...args){
-    return glslExtension.glslCoord(this,code,...args)
-}
-gS.glslCombine = function(code,texture,...args){
-    return glslExtension.glslCombine(this,code,texture,...args)
-}
-gS.glslCombineCoord = function(code,texture,...args){
-    return glslExtension.glslCombineCoord(this,code,texture,...args)
-}
+window.glsl = glslExtension.glslSource.bind(glslExtension);
+gS.glslColor = function (code, ...args) {
+  return glslExtension.glslColor(this, code, ...args);
+};
+gS.glslCoord = function (code, ...args) {
+  return glslExtension.glslCoord(this, code, ...args);
+};
+gS.glslCombine = function (code, texture, ...args) {
+  return glslExtension.glslCombine(this, code, texture, ...args);
+};
+gS.glslCombineCoord = function (code, texture, ...args) {
+  return glslExtension.glslCombineCoord(this, code, texture, ...args);
+};
