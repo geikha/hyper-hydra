f2493075ff41a61a5cf9af4746b4307f68538ed4
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2022-11-10 18:56:34 -0300

    add circle inversion

--- hydra-fractals.js
+++ hydra-fractals.js
@@ -1,48 +1,54 @@
 [{
-    name: 'mirrorX',
-    type: 'coord',
-    inputs: [
-      {
-        type: 'float',
-        name: 'pos',
-        default: 0.5,
-      }
-    ],
-    glsl: `_st.x = 0.0-abs(_st.x-pos)+0.5; return _st;`
-  },
-  {
-    name: 'mirrorY',
-    type: 'coord',
-    inputs: [
-      {
-        type: 'float',
-        name: 'pos',
-        default: 0.5,
-      }
-    ],
-    glsl: `_st.y = 0.0-abs(_st.y-pos)+0.5; return _st;`
-  },
-  {
-    name: 'mirrorX2',
-    type: 'coord',
-    inputs: [
-      {
-        type: 'float',
-        name: 'pos',
-        default: 0.5,
-      }
-    ],
-    glsl: `_st.y = abs(_st.y-pos)+0.5; return _st;`
-  },
-  {
-    name: 'mirrorY2',
-    type: 'coord',
-    inputs: [
-      {
-        type: 'float',
-        name: 'pos',
-        default: 0.5,
-      }
-    ],
-    glsl: `_st.y = abs(_st.y-pos)+0.5; return _st;`
-}].forEach((x)=>setFunction(x))
+  name: 'mirrorX',
+  type: 'coord',
+  inputs: [
+    {
+      type: 'float',
+      name: 'pos',
+      default: 0.5,
+    }
+  ],
+  glsl: `_st.x = 0.0-abs(_st.x-pos)+0.5; return _st;`
+},
+{
+  name: 'mirrorY',
+  type: 'coord',
+  inputs: [
+    {
+      type: 'float',
+      name: 'pos',
+      default: 0.5,
+    }
+  ],
+  glsl: `_st.y = 0.0-abs(_st.y-pos)+0.5; return _st;`
+},
+{
+  name: 'mirrorX2',
+  type: 'coord',
+  inputs: [
+    {
+      type: 'float',
+      name: 'pos',
+      default: 0.5,
+    }
+  ],
+  glsl: `_st.y = abs(_st.y-pos)+0.5; return _st;`
+},
+{
+  name: 'mirrorY2',
+  type: 'coord',
+  inputs: [
+    {
+      type: 'float',
+      name: 'pos',
+      default: 0.5,
+    }
+  ],
+  glsl: `_st.y = abs(_st.y-pos)+0.5; return _st;`
+},
+{
+  name: 'inversion',
+  type: 'coord',
+  inputs: [],
+  glsl: `_st /= dot(_st,_st); return _st;`
+}].forEach((x) => setFunction(x))
