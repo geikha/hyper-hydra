aaa76dc7a6634f2148e2dc4758a4eee97a074750
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2023-01-29 03:09:46 -0300

    update hydra-colorspaces

--- hydra-colorspaces.js
+++ hydra-colorspaces.js
@@ -3,6 +3,9 @@ window.gS = osc().constructor.prototype;
 gS.rgba = gS.color;
 gS.rgb = gS.color;
 
+window.rgb = window.solid;
+window.rgba = window.solid;
+
 window.hcs = {};
 
 hcs.colorscapes = [
@@ -34,7 +37,7 @@ hcs.colorscapes = [
     `,
   },
   {
-    name: 'hsl',
+    name: "hsl",
     elems: ["h", "s", "l"],
     to: `
         float cMin = min( _r, min( _g, _b ) );
@@ -54,11 +57,11 @@ hcs.colorscapes = [
         vec3 _rgb = clamp( abs(mod(h*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
         _rgb = l + s * (_rgb-0.5)*(1.0-abs(2.0*l-1.0));
         _r = _rgb.r; _g = _rgb.g; _b = _rgb.b;
-    `
+    `,
   },
   {
-    name: 'yuv',
-    elems: ['y','u','v'],
+    name: "yuv",
+    elems: ["y", "u", "v"],
     to: `
         mat3 rgb2yuv = mat3(0.2126, 0.7152, 0.0722, -0.09991, -0.33609, 0.43600, 0.615, -0.5586, -0.05639);
         vec3 _yuv = vec3(_r,_g,_b) * rgb2yuv;
@@ -68,11 +71,11 @@ hcs.colorscapes = [
         mat3 yuv2rgb = mat3(1.0, 0.0, 1.28033, 1.0, -0.21482, -0.38059, 1.0, 2.12798, 0.0);
         vec3 _rgb = vec3(y,u,v) * yuv2rgb;
         _r = _rgb.r; _g = _rgb.g; _b = _rgb.b;
-    `
+    `,
   },
   {
-    name: 'yiq',
-    elems: ['y','i','q'],
+    name: "yiq",
+    elems: ["y", "i", "q"],
     to: `
         mat3 rgb2yiq = mat3(0.299, 0.587, 0.114, 0.5959, -0.2746, -0.3213, 0.2115, -0.5227, 0.3112);
         vec3 _yiq = vec3(_r,_g,_b) * rgb2yiq;
@@ -82,8 +85,8 @@ hcs.colorscapes = [
         mat3 yiq2rgb = mat3(1.0, 0.956, 0.619, 1.0, -0.272, -0.647, 1.0, -1.106, 1.703);
         vec3 _rgb = vec3(y,i,q) * yiq2rgb;
         _r = _rgb.r; _g = _rgb.g; _b = _rgb.b;
-    `
-  }
+    `,
+  },
 ];
 
 hcs.generateColorFunction = function (colorspace) {
@@ -163,21 +166,23 @@ hcs.generateSolidFunction = function (colorspace) {
 };
 
 hcs.generateElementFunctions = function (colorspace) {
-    return colorspace.elems.map((el) => {
-        const name = colorspace.name + "_" + el;
-        const type = "color";
-        const inputs = [];
-        const declarations = colorspace.elems
-            .map((_el) => "float " + _el + ";\n")
-            .join("");
-        const glsl =
-            "float _r = _c0.r; float _g = _c0.g; float _b = _c0.b; float _a = _c0.a;" 
-        +   declarations
-        + colorspace.to
-        + "return vec4(vec3(" + el + "),1.0);"
-        return { name: name, type: type, inputs: inputs, glsl: glsl };
-    })
-}
+  return colorspace.elems.map((el) => {
+    const name = colorspace.name + "_" + el;
+    const type = "color";
+    const inputs = [];
+    const declarations = colorspace.elems
+      .map((_el) => "float " + _el + ";\n")
+      .join("");
+    const glsl =
+      "float _r = _c0.r; float _g = _c0.g; float _b = _c0.b; float _a = _c0.a;" +
+      declarations +
+      colorspace.to +
+      "return vec4(vec3(" +
+      el +
+      "),1.0);";
+    return { name: name, type: type, inputs: inputs, glsl: glsl };
+  });
+};
 
 hcs.update = function () {
   hcs.colorscapes.map(hcs.generateColorFunction).forEach((x) => setFunction(x));
@@ -185,7 +190,10 @@ hcs.update = function () {
     .map(hcs.generateColorOffsetFunction)
     .forEach((x) => setFunction(x));
   hcs.colorscapes.map(hcs.generateSolidFunction).forEach((x) => setFunction(x));
-  hcs.colorscapes.map(hcs.generateElementFunctions).flat(1).forEach((x) => setFunction(x));
+  hcs.colorscapes
+    .map(hcs.generateElementFunctions)
+    .flat(1)
+    .forEach((x) => setFunction(x));
 };
 
 hcs.update();
