5b90e74c75375503f96c0d86d65760df5697c04d
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2023-01-30 03:14:36 -0300

    get width and height directly from fbo

--- hydra-pixels.js
+++ hydra-pixels.js
@@ -2,25 +2,26 @@
 //read pixels from each hydra output
 //by RITCHSE
 
-window.regl = o0.regl
-regl.attributes.preserveDrawingBuffer = true
+window.regl = o0.regl;
+regl.attributes.preserveDrawingBuffer = true;
 window.oP = o0.constructor.prototype;
 
 oP.read = function (x = 0, y = 0, w = 1, h = 1) {
-    return regl.read({
-        framebuffer: this.fbos[this.pingPongIndex],
-        x: x,
-        y: y,
-        width: w,
-        height: h
-    })
-}
+  return regl.read({
+    framebuffer: this.fbos[this.pingPongIndex],
+    x: x,
+    y: y,
+    width: w,
+    height: h,
+  });
+};
 oP.readAll = function () {
-    return regl.read({
-        framebuffer: this.fbos[this.pingPongIndex],
-        x: 0,
-        y: 0,
-        width: window.width,
-        height: window.height
-    })
-}
+  const fbo = this.fbos[this.pingPongIndex];
+  return regl.read({
+    framebuffer: fbo,
+    x: 0,
+    y: 0,
+    width: fbo.width,
+    height: fbo.height,
+  });
+};
