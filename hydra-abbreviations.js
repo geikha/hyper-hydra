7a3965e5d18dd1c25a1a0a67a0abd8c7956ee453
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2023-01-30 03:11:47 -0300

    rename glslsource proto for consistency

--- hydra-abbreviations.js
+++ hydra-abbreviations.js
@@ -2,252 +2,246 @@
 //shorthands for all the funcs
 //by RITCHSE
 
-gs = osc().constructor.prototype
+window.gS = osc().constructor.prototype;
 
-gs.bl = gs.blend
-gs.bright = gs.brightness
-gs.br = gs.brightness
-gs.clr = gs.color
-gs.c = gs.color
-gs.clrm = gs.colorama
-gs.cm = gs.colorama
-gs.cntrst = gs.contrast
-gs.ct = gs.contrast
-gs.df = gs.diff
-gs.invrt = gs.invert
-gs.inv = gs.invert
-gs.i = gs.invert
-gs.kld = gs.kaleid
-gs.k = gs.kaleid
-gs.lyr = gs.layer
-gs.ly = gs.layer
-gs.lr = gs.layer
-gs.l = gs.layer
-gs.lm = gs.luma
-gs.msk = gs.mask
-gs.mod = gs.modulate
-gs.m = gs.modulate
-gs.modh = gs.modulateHue
-gs.mh = gs.modulateHue
-gs.mH = gs.modulateHue
-gs.modk = gs.modulateKaleid
-gs.mk = gs.modulateKaleid
-gs.mK = gs.modulateKaleid
-gs.modp = gs.modulatePixelate
-gs.mp = gs.modulatePixelate
-gs.mP = gs.modulatePixelate
-gs.modrp = gs.modulateRepeat
-gs.mrp = gs.modulateRepeat
-gs.mRp = gs.modulateRepeat
-gs.modrpx = gs.modulateRepeatX
-gs.mrpx = gs.modulateRepeatX
-gs.mRpx = gs.modulateRepeatX
-gs.mRpX = gs.modulateRepeatX
-gs.modrpy = gs.modulateRepeatY
-gs.mrpy = gs.modulateRepeatY
-gs.mRpy = gs.modulateRepeatY
-gs.mRpY = gs.modulateRepeatY
-gs.modr = gs.modulateRotate
-gs.mr = gs.modulateRotate
-gs.mR = gs.modulateRotate
-gs.modrt = gs.modulateRotate
-gs.mrt = gs.modulateRotate
-gs.mRt = gs.modulateRotate
-gs.modrot = gs.modulateRotate
-gs.mrot = gs.modulateRotate
-gs.mRot = gs.modulateRotate
-gs.mods = gs.modulateScale
-gs.ms = gs.modulateScale
-gs.mS = gs.modulateScale
-gs.modx = gs.modulateScrollX
-gs.mx = gs.modulateScrollX
-gs.mX = gs.modulateScrollX
-gs.mody = gs.modulateScrollY
-gs.my = gs.modulateScrollY
-gs.mY = gs.modulateScrollY
-gs.mlt = gs.mult
-gs.ml = gs.mult
-gs.mt = gs.mult
-gs.pxlt = gs.pixelate
-gs.px = gs.pixelate
-gs.p = gs.pixelate
-gs.pstr = gs.posterize
-gs.ps = gs.posterize
-gs.rpt = gs.repeat
-gs.rp = gs.repeat
-gs.rptx = gs.repeatX
-gs.rpx = gs.repeatX
-gs.rptX = gs.repeatX
-gs.rpX = gs.repeatX
-gs.rpty = gs.repeatY
-gs.rpy = gs.repeatY
-gs.rptY = gs.repeatY
-gs.rpY = gs.repeatY
-gs.rot = gs.rotate
-gs.rt = gs.rotate
-gs.strt = gs.saturate
-gs.st = gs.saturate
-gs.s = gs.scale
-gs.sc = gs.scale
-gs.scrll = gs.scroll
-gs.xy = gs.scroll
-gs.x = gs.scrollX
-gs.y = gs.scrollY
-gs.shft = gs.shift
-gs.sh = gs.shift
-gs.sb = gs.sub
-gs.sm = gs.sum
-gs.thrsh = gs.thresh
-gs.thr = gs.thresh
-gs.th = gs.thresh
-gs.t = gs.thresh
+gS.bl = gS.blend;
+gS.bright = gS.brightness;
+gS.br = gS.brightness;
+gS.clr = gS.color;
+gS.c = gS.color;
+gS.clrm = gS.colorama;
+gS.cm = gS.colorama;
+gS.cntrst = gS.contrast;
+gS.ct = gS.contrast;
+gS.df = gS.diff;
+gS.invrt = gS.invert;
+gS.inv = gS.invert;
+gS.i = gS.invert;
+gS.kld = gS.kaleid;
+gS.k = gS.kaleid;
+gS.lyr = gS.layer;
+gS.ly = gS.layer;
+gS.lr = gS.layer;
+gS.l = gS.layer;
+gS.lm = gS.luma;
+gS.msk = gS.mask;
+gS.mod = gS.modulate;
+gS.m = gS.modulate;
+gS.modh = gS.modulateHue;
+gS.mh = gS.modulateHue;
+gS.mH = gS.modulateHue;
+gS.modk = gS.modulateKaleid;
+gS.mk = gS.modulateKaleid;
+gS.mK = gS.modulateKaleid;
+gS.modp = gS.modulatePixelate;
+gS.mp = gS.modulatePixelate;
+gS.mP = gS.modulatePixelate;
+gS.modrp = gS.modulateRepeat;
+gS.mrp = gS.modulateRepeat;
+gS.mRp = gS.modulateRepeat;
+gS.modrpx = gS.modulateRepeatX;
+gS.mrpx = gS.modulateRepeatX;
+gS.mRpx = gS.modulateRepeatX;
+gS.mRpX = gS.modulateRepeatX;
+gS.modrpy = gS.modulateRepeatY;
+gS.mrpy = gS.modulateRepeatY;
+gS.mRpy = gS.modulateRepeatY;
+gS.mRpY = gS.modulateRepeatY;
+gS.modr = gS.modulateRotate;
+gS.mr = gS.modulateRotate;
+gS.mR = gS.modulateRotate;
+gS.modrt = gS.modulateRotate;
+gS.mrt = gS.modulateRotate;
+gS.mRt = gS.modulateRotate;
+gS.modrot = gS.modulateRotate;
+gS.mrot = gS.modulateRotate;
+gS.mRot = gS.modulateRotate;
+gS.mods = gS.modulateScale;
+gS.ms = gS.modulateScale;
+gS.mS = gS.modulateScale;
+gS.modx = gS.modulateScrollX;
+gS.mx = gS.modulateScrollX;
+gS.mX = gS.modulateScrollX;
+gS.mody = gS.modulateScrollY;
+gS.my = gS.modulateScrollY;
+gS.mY = gS.modulateScrollY;
+gS.mlt = gS.mult;
+gS.ml = gS.mult;
+gS.mt = gS.mult;
+gS.pxlt = gS.pixelate;
+gS.px = gS.pixelate;
+gS.p = gS.pixelate;
+gS.pstr = gS.posterize;
+gS.ps = gS.posterize;
+gS.rpt = gS.repeat;
+gS.rp = gS.repeat;
+gS.rptx = gS.repeatX;
+gS.rpx = gS.repeatX;
+gS.rptX = gS.repeatX;
+gS.rpX = gS.repeatX;
+gS.rpty = gS.repeatY;
+gS.rpy = gS.repeatY;
+gS.rptY = gS.repeatY;
+gS.rpY = gS.repeatY;
+gS.rot = gS.rotate;
+gS.rt = gS.rotate;
+gS.strt = gS.saturate;
+gS.st = gS.saturate;
+gS.s = gS.scale;
+gS.sc = gS.scale;
+gS.scrll = gS.scroll;
+gS.xy = gS.scroll;
+gS.x = gS.scrollX;
+gS.y = gS.scrollY;
+gS.shft = gS.shift;
+gS.sh = gS.shift;
+gS.sb = gS.sub;
+gS.sm = gS.sum;
+gS.thrsh = gS.thresh;
+gS.thr = gS.thresh;
+gS.th = gS.thresh;
+gS.t = gS.thresh;
 
-gs.__proto__.o = gs.__proto__.out
+gS.__proto__.o = gS.__proto__.out;
 
-window.n = noise
-window.ns = noise
-window.vor = voronoi
-window.vr = voronoi
-window.v = voronoi
-window.shp = shape
-window.sh = shape
-window.grd = gradient
-window.gr = gradient
-window.gd = gradient
-window.s = solid
-window.sl = solid
-window.o = osc
+window.n = noise;
+window.ns = noise;
+window.vor = voronoi;
+window.vr = voronoi;
+window.v = voronoi;
+window.shp = shape;
+window.sh = shape;
+window.grd = gradient;
+window.gr = gradient;
+window.gd = gradient;
+window.s = solid;
+window.sl = solid;
+window.o = osc;
 
 // abbreviator
 // TODO: refactor this mess:
 
 window.Abbreviator = {
-    abbreviations: {
-        functions: {
-            'blend': 'bl',
-            'bright': 'br',
-            'color': 'c',
-            'colorama': 'cm',
-            'contrast': 'ct',
-            'diff': 'df',
-            'invert': 'inv',
-            'kaleid': 'k',
-            'layer': 'l',
-            'luma': 'lm',
-            'mask': 'msk',
-            'modulate': 'm',
-            'modulateHue': 'mH',
-            'modulateKaleid': 'mK',
-            'modulatePixelate': 'mP',
-            'modulateRepeat': 'mRp',
-            'modulateRepeatX': 'mRpX',
-            'modulateRepeatY': 'mRpY',
-            'modulateRotate': 'mR',
-            'modulateScale': 'mS',
-            'modulateScrollX': 'mX',
-            'modulateScrollY': 'mY',
-            'mult': 'mlt',
-            'pixelate': 'p',
-            'posterize': 'ps',
-            'repeat': 'rp',
-            'repeatX': 'rpX',
-            'repeatY': 'rpY',
-            'rotate': 'rt',
-            'saturate': 'st',
-            'scale': 'sc',
-            'scroll': 'xy',
-            'scrollX': 'x',
-            'scrollY': 'y',
-            'shift': 'sh',
-            'sub': 'sb',
-            'sum': 'sm',
-            'thresh': 'th'
-        },
-        sources: {
-            'noise': 'ns',
-            'voronoi': 'vr',
-            'shape': 'sh',
-            'gradient': 'grd',
-            'solid': 'sl',
-        }
-    }, // here comes some ugly code:
-    getKeyFromValue(object,value) {
-        return Object.keys(object).find(key => object[key] === value);
+  abbreviations: {
+    functions: {
+      blend: "bl",
+      bright: "br",
+      color: "c",
+      colorama: "cm",
+      contrast: "ct",
+      diff: "df",
+      invert: "inv",
+      kaleid: "k",
+      layer: "l",
+      luma: "lm",
+      mask: "msk",
+      modulate: "m",
+      modulateHue: "mH",
+      modulateKaleid: "mK",
+      modulatePixelate: "mP",
+      modulateRepeat: "mRp",
+      modulateRepeatX: "mRpX",
+      modulateRepeatY: "mRpY",
+      modulateRotate: "mR",
+      modulateScale: "mS",
+      modulateScrollX: "mX",
+      modulateScrollY: "mY",
+      mult: "mlt",
+      pixelate: "p",
+      posterize: "ps",
+      repeat: "rp",
+      repeatX: "rpX",
+      repeatY: "rpY",
+      rotate: "rt",
+      saturate: "st",
+      scale: "sc",
+      scroll: "xy",
+      scrollX: "x",
+      scrollY: "y",
+      shift: "sh",
+      sub: "sb",
+      sum: "sm",
+      thresh: "th",
     },
-    checkAndUnabbreviateSource(match, offset, string) {
-        k = match.replace('(','')
-        replace = Abbreviator.getKeyFromValue(Abbreviator.abbreviations.sources,k) + '('
-        if (offset == 0)
-            return replace
-        pch = string[offset - 1]
-        if ("\n\t;( ".includes(pch))
-            return replace
-        else
-            return match
+    sources: {
+      noise: "ns",
+      voronoi: "vr",
+      shape: "sh",
+      gradient: "grd",
+      solid: "sl",
     },
-    checkAndUnabbreviateFunction(match, offset, string) {
-        k = match.replace('(','').replace('.','')
-        replace = '.' + Abbreviator.getKeyFromValue(Abbreviator.abbreviations.functions,k) +'('
-        if (offset == 0)
-            return match
-        pch = string[offset - 1]
-        if ("\n\t )".includes(pch))
-            return replace
-        else
-            return match
-    },
-    checkAndAbbreviateSource(match, offset, string) {
-        k = match.replace('(','')
-        replace = Abbreviator.abbreviations.sources[k] + '('
-        if (offset == 0)
-            return replace
-        pch = string[offset - 1]
-        if ("\n\t;( ".includes(pch))
-            return replace
-        else
-            return match
-    },
-    checkAndAbbreviateFunction(match, offset, string) {
-        k = match.replace('(','').replace('.','')
-        replace = '.' + Abbreviator.abbreviations.functions[k] +'('
-        if (offset == 0)
-            return match
-        pch = string[offset - 1]
-        if ("\n\t )".includes(pch))
-            return replace
-        else
-            return match
-    },
-    removeSpacing(code){
-        return code.replaceAll(' ','').replaceAll('\n','').replaceAll('\t','')
-    },
-    abbreviate(code,noSpacing=false) {
-        Object.keys(Abbreviator.abbreviations.functions)
-            .forEach((k) => {
-                code = code.replaceAll('.' + k + '(', Abbreviator.checkAndAbbreviateFunction)
-            })
-        Object.keys(Abbreviator.abbreviations.sources)
-            .forEach((k) => {
-                code = code.replaceAll(k + "(", Abbreviator.checkAndAbbreviateSource)
-            })
-        code = noSpacing ? Abbreviator.removeSpacing(code) : code
-        console.log(code)
-        return code
-    },
-    unabbreviate(code) {
-        Object.keys(Abbreviator.abbreviations.sources)
-            .forEach((k) => {
-                k = Abbreviator.abbreviations.sources[k]
-                code = code.replaceAll(k + "(", Abbreviator.checkAndUnabbreviateSource)
-            })
-        Object.keys(Abbreviator.abbreviations.functions)
-            .forEach((k) => {
-                k = Abbreviator.abbreviations.functions[k]
-                code = code.replaceAll('.' + k + '(', Abbreviator.checkAndUnabbreviateFunction)
-            })
-        console.log(code)
-        return code
-    }
-}
-window.abbreviate = Abbreviator.abbreviate.bind(Abbreviator)
-window.unabbreviate =Abbreviator.unabbreviate.bind(Abbreviator)
+  }, // here comes some ugly code:
+  getKeyFromValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  },
+  checkAndUnabbreviateSource(match, offset, string) {
+    k = match.replace("(", "");
+    replace =
+      Abbreviator.getKeyFromValue(Abbreviator.abbreviations.sources, k) + "(";
+    if (offset == 0) return replace;
+    pch = string[offset - 1];
+    if ("\n\t;( ".includes(pch)) return replace;
+    else return match;
+  },
+  checkAndUnabbreviateFunction(match, offset, string) {
+    k = match.replace("(", "").replace(".", "");
+    replace =
+      "." +
+      Abbreviator.getKeyFromValue(Abbreviator.abbreviations.functions, k) +
+      "(";
+    if (offset == 0) return match;
+    pch = string[offset - 1];
+    if ("\n\t )".includes(pch)) return replace;
+    else return match;
+  },
+  checkAndAbbreviateSource(match, offset, string) {
+    k = match.replace("(", "");
+    replace = Abbreviator.abbreviations.sources[k] + "(";
+    if (offset == 0) return replace;
+    pch = string[offset - 1];
+    if ("\n\t;( ".includes(pch)) return replace;
+    else return match;
+  },
+  checkAndAbbreviateFunction(match, offset, string) {
+    k = match.replace("(", "").replace(".", "");
+    replace = "." + Abbreviator.abbreviations.functions[k] + "(";
+    if (offset == 0) return match;
+    pch = string[offset - 1];
+    if ("\n\t )".includes(pch)) return replace;
+    else return match;
+  },
+  removeSpacing(code) {
+    return code.replaceAll(" ", "").replaceAll("\n", "").replaceAll("\t", "");
+  },
+  abbreviate(code, noSpacing = false) {
+    Object.keys(Abbreviator.abbreviations.functions).forEach((k) => {
+      code = code.replaceAll(
+        "." + k + "(",
+        Abbreviator.checkAndAbbreviateFunction
+      );
+    });
+    Object.keys(Abbreviator.abbreviations.sources).forEach((k) => {
+      code = code.replaceAll(k + "(", Abbreviator.checkAndAbbreviateSource);
+    });
+    code = noSpacing ? Abbreviator.removeSpacing(code) : code;
+    console.log(code);
+    return code;
+  },
+  unabbreviate(code) {
+    Object.keys(Abbreviator.abbreviations.sources).forEach((k) => {
+      k = Abbreviator.abbreviations.sources[k];
+      code = code.replaceAll(k + "(", Abbreviator.checkAndUnabbreviateSource);
+    });
+    Object.keys(Abbreviator.abbreviations.functions).forEach((k) => {
+      k = Abbreviator.abbreviations.functions[k];
+      code = code.replaceAll(
+        "." + k + "(",
+        Abbreviator.checkAndUnabbreviateFunction
+      );
+    });
+    console.log(code);
+    return code;
+  },
+};
+window.abbreviate = Abbreviator.abbreviate.bind(Abbreviator);
+window.unabbreviate = Abbreviator.unabbreviate.bind(Abbreviator);
