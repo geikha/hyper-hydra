4bc7298487eb6ad091eeee520070e563d78502e5
Renzo Torr- <56176668+ritchse@users.noreply.github.com>
2023-02-03 04:57:59 -0300

    scope & compatible with instance mode

--- hydra-pixels.js
+++ hydra-pixels.js
@@ -2,26 +2,54 @@
 //read pixels from each hydra output
 //by RITCHSE
 
-window.regl = o0.regl;
-regl.attributes.preserveDrawingBuffer = true;
-window.oP = o0.constructor.prototype;
+{
+    const getHydra = function () {
+        const whereami = window.choo?.state?.hydra
+            ? "editor"
+            : window.atom?.packages
+            ? "atom"
+            : "idk";
+        switch (whereami) {
+            case "editor":
+                return choo.state.hydra.hydra;
+            case "atom":
+                return global.atom.packages.loadedPackages["atom-hydra"]
+                    .mainModule.main.hydra;
+            case "idk":
+                let _h = undefined;
+                _h = window._hydra?.regl ? window._hydra : _h;
+                _h = window.hydra?.regl ? window.hydra : _h;
+                _h = window.h?.regl ? window.h : _h;
+                _h = window.H?.regl ? window.H : _h;
+                _h = window.hy?.regl ? window.hy : _h;
+                return h;
+        }
+    };
+    window._hydra = getHydra();
+}
 
-oP.read = function (x = 0, y = 0, w = 1, h = 1) {
-  return regl.read({
-    framebuffer: this.fbos[this.pingPongIndex],
-    x: x,
-    y: y,
-    width: w,
-    height: h,
-  });
-};
-oP.readAll = function () {
-  const fbo = this.fbos[this.pingPongIndex];
-  return regl.read({
-    framebuffer: fbo,
-    x: 0,
-    y: 0,
-    width: fbo.width,
-    height: fbo.height,
-  });
-};
+{
+    window.oP = _hydra.o[0].constructor.prototype;
+
+    const regl = _hydra.o[0].regl;
+    regl.attributes.preserveDrawingBuffer = true;
+    oP.read = function (x = 0, y = 0, w = 1, h = 1) {
+        return regl.read({
+            framebuffer: this.fbos[this.pingPongIndex],
+            x: x,
+            y: y,
+            width: w,
+            height: h,
+        });
+    };
+    oP.readAll = function () {
+        const fbo = this.fbos[this.pingPongIndex];
+        return regl.read({
+            framebuffer: fbo,
+            x: 0,
+            y: 0,
+            width: fbo.width,
+            height: fbo.height,
+        });
+    };
+}
