c768b3caf4dd5245d57c65926dfde7d9e1817aed
GEIKHA <56176668+geikha@users.noreply.github.com>
2023-10-10 18:53:14 -0300

    hydra-src update

--- hydra-src.js
+++ hydra-src.js
@@ -117,4 +117,38 @@
       				.rotate(...args)
       				.scale(1,canvas.clientHeight/canvas.clientWidth)
     }
+    gS.correctScaleRel = function(source){
+        const tex = source;
+        if (!tex.hasOwnProperty("src")) return src(tex);
+        const w = () =>
+            tex.src?.width
+                ? tex.src?.width / tex.src?.height
+                : tex.src?.videoWidth
+                ? tex.src?.videoWidth / tex.src?.videoHeight
+                : 0;
+        const h = () =>
+            tex.src?.height
+                ? tex.src?.height / tex.src?.width
+                : tex.src?.videoHeight
+                ? tex.src?.videoHeight / tex.src?.videoWidth
+                : 0;
+        const cw = () => canvas.clientWidth / canvas.clientHeight;
+        const ch = () => canvas.clientHeight / canvas.clientWidth;
+        return this.scale(
+            1,
+            () => {
+                const _cw = cw();
+                const _w = w();
+                return _cw > _w ? _w / _cw : 1;
+            },
+            () => {
+                const _ch = ch();
+                const _h = h();
+                return _ch > _h ? _h / _ch : 1;
+            }
+        );
+    }
+    gS.noWrap = function(sm=0){
+        return this.mask(shape(4, 1, sm));
+    }
 }
